e5e9bae04e2c90f49c28c9c3e1b12da7c2914c2b
Ali Khajegili Mirabadi <52541800+AliKhajegiliM@users.noreply.github.com>
2020-03-09 17:36:48 +0330

    Add files via upload

--- imhist2D.m
+++ imhist2D.m
@@ -1,4 +1,8 @@
 function h_ij=imhist2D(X1,X2)
+% What is 2D histogram:
+%The count of same position pixels which for pair (i,j), contains intensity- 
+% level i in first image, and similarly intensity-level j for second image.
+
 %X1 and X2 two 2D same size input images or channels (N*M).
 % This function divide by resolution is an "approximation" for joint pmf.
 % Developed by Ali Khajegili Mirabadi
@@ -7,10 +11,10 @@ function h_ij=imhist2D(X1,X2)
 if size(X1,3)>1
     disp('Input Signal Dimension Is Not Correct: Input Must Be 2D Image');
 else
-[row,col]=size(X1); % The count of rows or N % The count of columns or M
+[N,M]=size(X1); % The count of rows or N % The count of columns or M
 h_ij=zeros(256,256); %primitive 2D joint histogram function
-for r=1:row   
-  for c=1:col   
+for r=1:N   
+  for c=1:M 
     h_ij(X1(r,c)+1,X2(r,c)+1)= h_ij(X1(r,c)+1,X2(r,c)+1)+1;
   end
 end
